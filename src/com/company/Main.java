5a8a19fa46acc1486f14da58f84177d2bb95bd7f
Ptchorzewski <ptchorzewski@student.kul.lublin.pl>
2022-03-14 18:34:57 +0100

    changes in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Linia z gałęzi secondBranch");
+        System.out.println("2 linia z gałęzi secondBranch");
     }
 }
